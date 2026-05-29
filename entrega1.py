565460b04d5d391719ba0bfc096f07a2e84f1698
EmanuelBoscacci <boscacciemanuel@gmail.com>
2026-05-29 15:21:38 -0300

    Creacion de Cost

--- entrega1.py
+++ entrega1.py
@@ -20,7 +20,7 @@ class Rover(SearchProblem):
 
         acciones = []
 
-        # 1. Variables de estado claras para simplificar los 'if'
+ 
         en_ignea = posicion in muestras_igneas
         en_sedimentaria = posicion in muestras_sedimentarias
         en_sombra = posicion in zonas_sombra
@@ -29,7 +29,7 @@ class Rover(SearchProblem):
         mochila_vacia = len(mochila) == 0
         faltan_muestras = (len(muestras_igneas) + len(muestras_sedimentarias)) > 0
 
-        # 2. Acciones de Movimiento
+ 
         if bateria > 1:
             for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 acciones.append(("moverse", (posicion[0] + dx, posicion[1] + dy)))
@@ -38,31 +38,49 @@ class Rover(SearchProblem):
             for dx, dy in [(0, 2), (0, -2), (2, 0), (-2, 0)]:
                 acciones.append(("sobremarcha", (posicion[0] + dx, posicion[1] + dy)))
 
-        # 3. Acción de Recarga
+ 
         if bateria < 20 and not en_sombra:
             acciones.append(("recargar", None))
 
-        # 4. Acciones en zona de Muestras Ígneas
         if en_ignea and bateria > 1:
             if taladro != "termico":
                 acciones.append(("equipar", "termico"))
             elif bateria > 3 and not mochila_llena:
                 acciones.append(("recolectar", "ignea"))
 
-        # 5. Acciones en zona de Muestras Sedimentarias
         if en_sedimentaria and bateria > 1:
             if taladro != "percusion":
                 acciones.append(("equipar", "percusion"))
             elif bateria > 3 and not mochila_llena:
                 acciones.append(("recolectar", "sedimentaria"))
 
-        # 6. Acción de Depositar
         if not mochila_vacia and bateria > 1:
             if mochila_llena or not faltan_muestras:
                 acciones.append(("depositar", None))
 
         return acciones
+    
+    def cost(self, state1, action, state2):
+        tipo_accion = action[0]
+        
+        if tipo_accion in ("moverse", "sobremarcha"):
+            return 1 
+            
+        elif tipo_accion == "recolectar":
+            return 2
+            
+        elif tipo_accion == "equipar":
+            return 3
+            
+        elif tipo_accion == "recargar":
+            return 4
+            
+        elif tipo_accion == "depositar":
 
+            muestras_almacenadas = state1[6]
+            return len(muestras_almacenadas)
+            
+        return 0
 
 
 
